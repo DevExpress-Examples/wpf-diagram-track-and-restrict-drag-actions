124b95933f972132e8124a9c43ecaef0b40ac721
SH-Kirill <42139135+SH-Kirill@users.noreply.github.com>
2023-12-26 17:33:57 +0400

    Add files via upload

---
+++ CS/WpfApp7/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+       
+    }
+}
